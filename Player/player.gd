e665c78b66a402a834dea8466450809bbd4d2ffa
olioniT <olionig12@gmail.com>
2024-05-14 16:44:00 +1200

    improved player movement, collision being worked on

--- Player/player.gd
+++ Player/player.gd
@@ -1,17 +1,17 @@
 extends CharacterBody2D
 
+@export var MAX_VELOCITY_X = 200
+@export var ACCELERATION_X = 25
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+@export var MAX_VELOCITY_Y = 300
+@export var ACCELERATION_Y = 30
+
+@export var JUMP_VELOCITY = 400.0
+@export var GRAVITY = 9
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-
 func _physics_process(delta):
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	velocity.x = move_toward(velocity.x, Input.get_axis("move_left", "move_right") * MAX_VELOCITY_X, ACCELERATION_X)
+	velocity.y = move_toward(velocity.y, MAX_VELOCITY_Y, ACCELERATION_Y)
 	move_and_slide()
