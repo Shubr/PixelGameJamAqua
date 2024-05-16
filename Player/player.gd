fb2646af092fd307d50440ec129deff30956ea52
olioniT <olionig12@gmail.com>
2024-05-16 15:25:59 +1200

    movement is now working

--- Player/player.gd
+++ Player/player.gd
@@ -1,24 +1,20 @@
 extends CharacterBody2D
 
-var VELOCITY = Vector2(0, 0)
+var VELOCITY = Vector2.ZERO
 
-@export var MAX_VELOCITY_X = 5
-@export var ACCELERATION_X = 0.5
+@export var MAX_VELOCITY_X = 50
 
-@export var MAX_VELOCITY_Y = 9.8
-@export var ACCELERATION_Y = 0.1
+@export var ACCELERATION_X = 10
+@export var ACCELERATION_Y = 2.5
 
-@export var JUMP_VELOCITY = 400.0
-@export var GRAVITY = 20
+@export var JUMP_VELOCITY = 75.0
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _physics_process(delta):
+	move_and_slide()
+	velocity.x = move_toward(velocity.x, Input.get_axis("move_left", "move_right") * MAX_VELOCITY_X, ACCELERATION_X)
+	velocity.y = move_toward(velocity.y, gravity, ACCELERATION_Y)
 	
-	var collision = move_and_collide(VELOCITY * delta)
-	
-	VELOCITY.x = move_toward(VELOCITY.x, Input.get_axis("move_left", "move_right") * MAX_VELOCITY_X, ACCELERATION_X)
-	VELOCITY.y = move_toward(VELOCITY.y, MAX_VELOCITY_Y, ACCELERATION_Y)
-	
-	if collision:
-		print("PLAYER, colldied with ", collision.get_collider().name)
+	if Input.is_action_pressed("jump") and is_on_floor():
+		velocity.y -= JUMP_VELOCITY
